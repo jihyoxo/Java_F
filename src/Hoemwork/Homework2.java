b2e384f8dc8b338e854c6b4d321811c5a71ea505
jihyoxo <jkim41@risd.edu>
2019-11-22 18:29:41 +0900

    2019-11-25-v1

--- src/Hoemwork/Homework2.java
+++ src/Hoemwork/Homework2.java
@@ -2,11 +2,29 @@ package Hoemwork;
 
 public class Homework2 {
 	public static void main(String[] args) {
-		//문자 reverse 하기
+		//문자 reverse 하기.
 		//1. input "abcd"		=> 	"dcba"
 		//2. input "abcde"		=>		"edcba"
 		
 		String input = "abcd";	//"abcde"
-		char[] ch = input.toCharArray();
+		char[] ch = input.toCharArray();	
+		
+		for (int i = ch.length -1 ; i >= 0 ; i--) {
+			System.out.print(ch[i]);
+		}
+		System.out.println();
+	
+		
+		//1~100까지 소수를 구하기.
+ 
+		int num = 100;
+		int rmd = 0;
+		int n = 0 ;
+		for (n = 1; n <= num; n++) {
+			if(num % n == 0){
+				System.out.println(num);
+			}
+		}
+		
 	}
 }
